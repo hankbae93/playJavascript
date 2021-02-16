42cec8d61d11d84c0156a7a7947b8d4bed278b50
Ranja Func <seolin9340@gmail.com>
2021-02-16 09:39:31 +0900

    Fix TicTacToe to check winning logic completely

--- 07_ticTacToe/app.js
+++ 07_ticTacToe/app.js
@@ -1,54 +1,52 @@
 'use strict';
-const row = [...document.querySelectorAll('.row')];
-const box = Array(row.length)
+const rows = [...document.querySelectorAll('.row')];
+const boxes = Array(rows.length)
     .fill()
-    .map((box, i) => box = [...row[i].querySelectorAll('.box')]);
+    .map((box, i) => box = [...rows[i].querySelectorAll('.box')]);
 const BOX = document.querySelectorAll('.box');
 let turn = 'X';
 let fullCheck = 0;
 let win = false;
 
-function winCheckLogic(turn) {   
-    if (box[0][0].textContent === turn &&
-        box[0][1].textContent === turn &&
-        box[0][2].textContent === turn
-    ) {
+function winCheckLogic(turn, rowIndex, colIndex) {  
+    //가로줄 검사       
+    if (boxes[rowIndex][0].textContent === turn && 
+        boxes[rowIndex][1].textContent === turn &&
+        boxes[rowIndex][2].textContent === turn) { 
             win = true;
-    } else if (box[1][0].textContent === turn &&
-        box[1][1].textContent === turn &&
-        box[1][2].textContent === turn
-    ) {
-            win = true;
-    } else if (box[2][0].textContent === turn &&
-        box[2][1].textContent === turn &&
-        box[2][2].textContent === turn
-    ) {
-            win = true;
-    } else if (box[2][0].textContent === turn &&
-        box[1][1].textContent === turn &&
-        box[0][2].textContent === turn
-    ) {
-            win = true;
-    } else if (box[0][0].textContent === turn &&
-        box[1][1].textContent === turn &&
-        box[2][2].textContent === turn
-    ) {
-            win = true;
+    // 세로줄 검사
+    } else if (boxes[0][colIndex].textContent === turn && 
+            boxes[1][colIndex].textContent=== turn &&
+            boxes[2][colIndex].textContent === turn) { 
+                win = true;
+    // 대각선 검사
+    } else if (boxes[0][0].textContent === turn && 
+            boxes[1][1].textContent=== turn &&
+            boxes[2][2].textContent === turn) { 
+                win = true;    
+    } else if (boxes[2][0].textContent === turn && 
+            boxes[1][1].textContent=== turn &&
+            boxes[0][2].textContent === turn) { 
+                win = true;    
+    // 무승부 
     } else if (fullCheck === BOX.length) {
-        alert('비기셨습니다.')
-    }    
+        alert('! 무승부 !');
+    }
 }
 
 
 BOX.forEach((item) => {
     item.addEventListener('click', (e) => {     
         if (win) return;
-       
+        const rowIndex = rows.indexOf(e.target.parentNode);
+        const colIndex = boxes[rowIndex].indexOf(e.target);
+        console.log(`${rowIndex}번째 줄 ${colIndex}번째 칸`);
+
         if (item.textContent === '') {
             item.textContent = turn;
             item.classList.add('clicked');
             fullCheck++;
-            winCheckLogic(turn); 
+            winCheckLogic(turn, rowIndex, colIndex); 
             if (!win) {
                 if (turn === 'X') {
                     turn = 'O';
@@ -56,7 +54,7 @@ BOX.forEach((item) => {
                     turn = 'X';
                 } 
             } else {
-                alert(`${turn} 님의 승리! VVVVVVVVVV`);
+                alert(`${turn} 님의 승리!`);
                 turn = 'X';
             }                          
         } 
